778843da97f7d79183b3456dca6ab0b3006ec480
sameerkhanal209 <89799263+sameerkhanal209@users.noreply.github.com>
2023-02-04 01:38:07 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,15 +37,17 @@
 		}
 	});
 
-	console.log($(window).width())
-	if ($(window).width() <= 750) {
-		$(document).on('scroll', function(){
-			if ( $(window).scrollTop() > 30) {
-				$('.header_navigation').addClass('navbar_active');
-			} else {
-				$('.header_navigation').removeClass('navbar_active');
-			}
-		});
+	if($('.header_ch')){
+		if ($(window).width() <= 750) {
+			
+			$(document).on('scroll', function(){
+				if ( $(window).scrollTop() > 30) {
+					$('.header_navigation').addClass('navbar_active');
+				} else {
+					$('.header_navigation').removeClass('navbar_active');
+				}
+			});
+		}
 	}
 
 })(jQuery);
